ad58b1edfa5225804d38fdb98212cb43dd1efbbc
Archit Gupta <gupta.archit01@gmail.com>
2023-07-03 17:21:16 +0530

    added

--- index.js
+++ index.js
@@ -8,25 +8,27 @@ const  architMiddleware = (req,res,next)=>{
   // console.log(req)
 } // Express middleware refers to a set of functions that execute during the processing of HTTP requests received by an Express application.
 
-app.use(express.static(path.join(__dirname,"public")))
+app.use(express.static(path.join(__dirname,"static")))
+// app.use(express.static(path.join(__dirname,"templates")))
+app.use('/',require(path.join(__dirname,'routes/blog.js')))
 app.use(architMiddleware)
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname,'index.html'))
 
-})
-app.get('/hello/:name', (req, res) => {
-  res.send('Hello World!' + req.params.name)
+// })
+// app.get('/hello/:name', (req, res) => {
+//   res.send('Hello World!' + req.params.name)
 
-})
-app.get('/about', (req, res) => {
-//   res.send('about')
-  res.sendFile(path.join(__dirname,'index.html'))
-  res.status(501)
-  res.json({"archit":34})
+// })
+// app.get('/about', (req, res) => {
+// //   res.send('about')
+//   res.sendFile(path.join(__dirname,'index.html'))
+//   res.status(501)
+//   res.json({"archit":34})
 
-})
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)

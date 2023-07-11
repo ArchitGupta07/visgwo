73aedc4bc0478b86d8d4f3cd84d87a2711d1f5ac
Archit Gupta <gupta.archit01@gmail.com>
2023-07-11 17:58:22 +0530

    Added

--- mycontacts-backend/server.js
+++ mycontacts-backend/server.js
@@ -1,6 +1,7 @@
 console.log("I am in an express project")
 
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
 const dotenv = require("dotenv").config();
 // const path = require('path');
 const app = express()
@@ -11,6 +12,8 @@ const port = process.env.PORT || 3000;
 
 
 app.use(express.json());
+
+app.use(errorHandler);
 
 
 app.use("/api/contacts", require("./routes/contactRoutes"));
